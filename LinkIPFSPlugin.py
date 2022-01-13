5749b0e3f42af2631f360b1e635ee01eea1e6a32
Alexandr <alexandrv.prosin@gmail.com>
2022-01-14 01:34:00 +0200

    Supported local IPFS node

--- LinkIPFSPlugin.py
+++ LinkIPFSPlugin.py
@@ -7,13 +7,24 @@ from Plugin import PluginManager
 from Config import config
 from Debug import Debug
 
+from urllib.request import urlopen, Request
+from urllib.parse import quote
+from urllib.error import URLError, HTTPError
+
 @PluginManager.registerTo("UiRequest")
 class UiRequestPlugin(object):
     def actionSiteMedia(self, path, **kwargs):
         if path.startswith("/media/ipfs/") or path.startswith("/media/ipns/"):
         	path_parts = self.parsePath(path)
         	ipfs_path = "/%s/%s" % (path_parts["address"], path_parts["inner_path"])
-        	return self.actionRedirect302("https://ipfs.io%s" % (ipfs_path))
+        	local_url = "http://127.0.0.1:8080%s" % (ipfs_path)
+        	gateway_url = "https://ipfs.io%s" % (ipfs_path)        	
+        	try:
+        		request = Request(local_url)
+        		response = urlopen(request, timeout=1)
+        		return self.actionRedirect302(local_url)
+        	except (HTTPError, URLError) as err:
+        		return self.actionRedirect302(gateway_url)
         return super(UiRequestPlugin, self).actionSiteMedia(path, **kwargs)
     def actionRedirect302(self, url):
         self.start_response('302 Redirect', [('Location', str(url))])
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
+# Intro
+Site developers can use `/ipfs/<address>` and `/ipns/<address>` links by hosting in own domain, setting an IPFS gateway. But how can it be implemeted in ZeroNet?
+
 # LinkIPFS
-Resolves `/ipfs/<address>` and `/ipns/<address>` links in ZeroNet sites.
-In the current implementation, just redirects to [ipfs.io](https://ipfs.io).
+Resolves `/ipfs/<address>` and `/ipns/<address>` links in ZeroNet sites, redirecting to local IPFS gateway (if it's available via port 8080 and "always Tor mode" is not selected) or, alternatively, to [ipfs.io](https://ipfs.io).
 
 Used [FilePack](https://github.com/HelloZeroNet/ZeroNet/tree/py3/plugins/FilePack) as an example.
+
+# TODO
+It should be build-in plugin in all mainstream ZeroNet forks.
+Check links like /ipfs/<hash>/file.zip/1 and /ipfs/<hash>/.
